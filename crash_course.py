009b8513fdb1ba4548d5ef94126c432d9b4b0c41
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-11-26 10:41:21 +0300

    codes for a crash course

---
+++ crash_course.py
@@ -0,0 +1,17 @@
+# Your first Python Program
+print("Hello, World!")
+
+#Variables and data types
+# Integer
+age = 25
+
+# Float
+height = 5.9
+
+# String
+name = "Dhanian"
+
+# Boolean
+is_student = True
+
+print(age, height, name, is_student)
